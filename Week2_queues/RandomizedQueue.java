3d31af83d4d739f3d616c131d6d00a9e1273cc06
Foriwaa <30636817+Foriwaa@users.noreply.github.com>
2018-06-25 15:55:38 -0400

    Update RandomizedQueue.java

--- Week2_queues/RandomizedQueue.java
+++ Week2_queues/RandomizedQueue.java
@@ -1,7 +1,6 @@
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
-//import java.lang;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
